7ca4549e1fd079bf5e1086d32a3250d908bc374f
voldarskiy@gmail.com <voldarskiy@gmail.com@7044fa5e-59cb-5108-da3d-dcc98bd9e096>
2012-05-15 17:02:49 +0000

    Тестовая программка для выбора треугольника курсором.

---
+++ trunk/DimensionX/PickerTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PickerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mapDraw3d1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mapDraw3d1.UpdatePicking(e.X, e.Y);
+            label1.Text = string.Format("глюк: {2}  -   курсор: x: {0}  y: {1}", e.X, e.Y, !mapDraw3d1.m_bCamMode);
+        }
+
+        private void mapDraw3d1_Click(object sender, EventArgs e)
+        {
+            mapDraw3d1.m_bCamMode = !mapDraw3d1.m_bCamMode;
+        }
+    }
+}
