149a0e79a057732a0d6b2b6613e948816a073ce9
voldarskiy@gmail.com <voldarskiy@gmail.com@7044fa5e-59cb-5108-da3d-dcc98bd9e096>
2012-05-15 17:02:49 +0000

    Тестовая программка для выбора треугольника курсором.

---
+++ DimensionX/PickerTest/DumbCamera.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PickerTest
+{
+    public class DumbCamera
+    {
+        public Vector3 Position { get; set; }
+      
+        public Matrix View { get; set; }
+        public Matrix Projection { get; set; }
+
+        public float m_fDistance;
+        
+        protected GraphicsDevice GraphicsDevice { get; set; }
+        private void generatePerspectiveProjectionMatrix(float FieldOfView)
+        {
+            PresentationParameters pp = GraphicsDevice.PresentationParameters;
+            this.Projection = Matrix.CreatePerspectiveFieldOfView(FieldOfView, GraphicsDevice.Viewport.AspectRatio, 0.1f, 100000f);
+        }
+        
+        public Vector3 Direction { get; private set; }
+
+        public DumbCamera(GraphicsDevice graphicsDevice)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            generatePerspectiveProjectionMatrix(MathHelper.PiOver4);
+
+            this.Position = new Vector3(0, 50000, 0);
+
+            m_fDistance = Vector3.Distance(Position, Vector3.Zero);
+        }
+
+        public void Update(bool bMode)
+        {
+            Matrix cameraRotation = Matrix.CreateFromYawPitchRoll(0, bMode? 0 : MathHelper.ToRadians(270), 0);
+
+            Direction = Vector3.Transform(bMode? Vector3.Down : Vector3.Forward, cameraRotation);
+            Position = Vector3.Zero - Direction * m_fDistance;
+
+            Vector3 cameraRotatedUpVector = Vector3.Transform(bMode? Vector3.Forward : Vector3.Up, cameraRotation);
+
+            View = Matrix.CreateLookAt(Position, Vector3.Zero, cameraRotatedUpVector);
+        }
+    }
+}
---
+++ DimensionX/PickerTest/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PickerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapDraw3d1 = new PickerTest.MapDraw3d();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mapDraw3d1
+            // 
+            this.mapDraw3d1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapDraw3d1.Location = new System.Drawing.Point(0, 0);
+            this.mapDraw3d1.Name = "mapDraw3d1";
+            this.mapDraw3d1.Size = new System.Drawing.Size(284, 262);
+            this.mapDraw3d1.TabIndex = 0;
+            this.mapDraw3d1.Text = "mapDraw3d1";
+            this.mapDraw3d1.Click += new System.EventHandler(this.mapDraw3d1_Click);
+            this.mapDraw3d1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapDraw3d1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(0, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 290);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mapDraw3d1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MapDraw3d mapDraw3d1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
