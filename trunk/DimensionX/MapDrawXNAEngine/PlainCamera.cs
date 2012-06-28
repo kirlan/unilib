743ef85e605ac856c28a6b7c41c968503ace416f
voldarskiy@gmail.com <voldarskiy@gmail.com@7044fa5e-59cb-5108-da3d-dcc98bd9e096>
2012-06-28 21:11:45 +0000

    1. Увеличена детализация выводимых текстур. 2. Изменеы параметры смешения текстур для лесных регионов. 3. Убран неиспользуемый мусор из класса камеры.

--- trunk/DimensionX/MapDrawXNAEngine/PlainCamera.cs
+++ trunk/DimensionX/MapDrawXNAEngine/PlainCamera.cs
@@ -14,8 +14,6 @@ namespace MapDrawXNAEngine
         public float Pitch { get; set; }
         public float Roll { get; set; }
 
-        private Vector3 translationPan;
-
         public PlainCamera(GraphicsDevice graphicsDevice)
             : base(graphicsDevice)
         {
@@ -23,8 +21,6 @@ namespace MapDrawXNAEngine
             this.Yaw = MathHelper.ToRadians(0);
             this.Pitch = MathHelper.ToRadians(270);
             this.Roll = MathHelper.ToRadians(0);
-
-            translationPan = Vector3.Zero;
 
             m_fDistance = Vector3.Distance(Position, Target);
         }
@@ -41,8 +37,6 @@ namespace MapDrawXNAEngine
 
         public override void Pan(float fLeft, float fUp)
         {
-            Vector3 pMove = new Vector3(fLeft, 0, fUp);
-            this.translationPan += pMove;
         }
 
         public override void ZoomIn(float fDistance)
@@ -64,31 +58,14 @@ namespace MapDrawXNAEngine
 
             Matrix cameraRotation = Matrix.CreateFromYawPitchRoll(Yaw, Pitch, Roll);
 
-            // Offset the position and reset the translation
-            translationPan = Vector3.Transform(translationPan, Matrix.CreateRotationY(Yaw));
-            Target += translationPan;
-
-            translationPan = Vector3.Zero;
-
             Target = Vector3.Transform(Target, Matrix.CreateScale(1, 0, 1));
+            Direction = Vector3.Transform(Vector3.Forward, cameraRotation);
 
-            Vector3 pOldDirection = Direction;
+            Vector3 cameraPos = Target - Direction * m_fDistance;
+            Position = cameraPos;
 
-            Vector3 cameraOriginalTarget = Vector3.Forward;
-            Direction = Vector3.Transform(cameraOriginalTarget, cameraRotation);
-
-            Vector3 cameraOrbit = Vector3.Zero;
-
-            cameraOrbit = Target - Direction * m_fDistance;
-
-            Position = cameraOrbit;
-
-            Vector3 cameraFinalTarget = Target;
-
-            Vector3 cameraOriginalUpVector = Vector3.Up;
-            Vector3 cameraRotatedUpVector = Vector3.Transform(cameraOriginalUpVector, cameraRotation);
-
-            View = Matrix.CreateLookAt(Position, cameraFinalTarget, cameraRotatedUpVector);
+            Vector3 cameraUp = Vector3.Transform(Vector3.Up, cameraRotation);
+            View = Matrix.CreateLookAt(Position, Target, cameraUp);
         }
     }
 }
